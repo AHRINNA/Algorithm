46d0c3e6dd130d5fe34e54fe97119e743d3a7b90
MW <53636986+arhrina@users.noreply.github.com>
2019-09-28 10:59:24 +0900

    어떤 정신병자가 출력을 저렇게 하냐..

---
+++ JUNGOL/ADVANCE/1291/1291.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int s, e;
+
+	while (true) {
+		cin >> s;
+		if (s < 2 || s > 9) {
+			cout << "INPUT ERROR!" << endl;
+			continue;
+		}
+		else
+			break;
+	}
+	while(true){
+
+		cin >> e;
+		if (e < 2 || e > 9) {
+			cout << "INPUT ERROR!" << endl;
+			continue;
+		}
+		else
+			break;
+	}
+
+	if (s > e) {
+		for (int i = s; i >= e; i--) {
+			for (int j = 1; j < 10; j++) {
+				printf("%d * %d = %2d\n", i, j, i * j);
+			}
+		}
+	}
+	else {
+		for (int i = s; i <= e; i++) {
+			for (int j = 1; j < 10; j++) {
+				printf("%d * %d = %2d\n", i, j, i * j);
+			}
+			cout << "   ";
+		}
+	}
+
+
+	return 0;
+}
