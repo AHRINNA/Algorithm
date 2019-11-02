2f302e0d72772f68f22a1213c2bc44e08a39928d
MW <53636986+arhrina@users.noreply.github.com>
2019-11-02 21:16:25 +0900

    cpp도 만들어야함

---
+++ SWexpert/D3/8821/8821.java
@@ -0,0 +1,30 @@
+import java.util.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution{
+
+public static void main(String args[]) throws Exception{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+int t = Integer.valueOf(br.readLine());
+for(int i = 0; i < t; i++){
+String str = br.readLine();
+if(str.isEmptry())
+return;
+
+}
+}
+
+public void 
+
+public boolean check(int i){
+return false;
+}
+
+public void delete(int i){
+
+}
+
+/*
+String toChararray를 이용해서 String을 하나하나의 캐릭터로 자르고 이것들을 카운트해서(boolean으로 그때마다 체크하든가 하나의 String에서 %2로
+나머지를 확인하든가해서 처리
+*/
