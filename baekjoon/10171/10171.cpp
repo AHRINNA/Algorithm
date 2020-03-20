616543aabea3f6fbdc56a61c0ab2a7a1a5a9de29
MW <hyoukim@naver.com>
2020-03-20 09:24:37 +0900

    Create 10171.cpp

---
+++ baekjoon/10171/10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\\    /\\ \n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|";
+    return 0;
+}
+// escape 문자열 처리가 필요
