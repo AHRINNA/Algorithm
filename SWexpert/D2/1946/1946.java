ca3a38a0785ec6e36733d27e6507354c331980d0
MW <hyoukim@hanmail.net>
2021-01-05 15:16:45 +0900

    주석체크

--- SWexpert/D2/1946/1946.java
+++ SWexpert/D2/1946/1946.java
@@ -7,6 +7,50 @@ class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
+        int T = Integer.valueOf(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.valueOf(br.readLine());
+            int l = 0;
+            String st = null;
+            int cnt = 0;
+            for (int j = 0; ; j++) {
+                for (int k = 0; k < 10; k++) {
+                    if (l == 0) {
+                        if(cnt == N) {
+                            break;
+                        }
+                        String[] s = br.readLine().split(" ");
+                        st = s[0];
+                        l = Integer.valueOf(s[1]);
+                        cnt++;
+                    }
+                    sb.append(st);
+                    l--;
+                }
+                if(l == 0) {
+                    if(cnt == N) {
+                        break;
+                    }
+                }
+                sb.append("\n");
+            }
+            System.out.println("#" + (i + 1) + " " + "\n" + sb);
+            sb.setLength(0);
+        }
+    }
+}
+
+/*
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
         String[] s = new String[2];
         StringBuilder st = new StringBuilder();
         int T = Integer.valueOf(br.readLine());
@@ -14,7 +58,7 @@ class Solution {
         for (int i = 0; i < T; i++) {
             N = Integer.valueOf(br.readLine());
             l = 0;
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++) { // 잘못된 조건. Java Runtime Error 발생
                 for (int k = 0; k < 10; k++) {
                     if (l == 0) {
                         if((j + 1) == N) {
@@ -35,3 +79,6 @@ class Solution {
         }
     }
 }
+
+
+*/
